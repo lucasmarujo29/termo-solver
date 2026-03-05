6ef041fc309f951270e1ede7e86afbe6187b82e1
Lucas Ferreira Marinho <185864460+lucasmarujo29@users.noreply.github.com>
2026-03-05 13:05:46 -0300

    filter v1

--- data/filter.py
+++ data/filter.py
@@ -7,19 +7,27 @@
 # 2. remover letras maiusculas, caracteres especiais, sendo acentos, hífens e etc
 # 3. remover palavras que não tenham exatamente 5 letras
 #
+import unidecode
 
 dict_base = open("dicitonary.txt", "r")
 dict_final = open("dictionary_modified.txt", "w")
 
 
 for line in dict_base:
-    line01 = line.strip()
-    
+    line = line.strip()
     #remover tag
-        
+    posSlash = line.find("/")
+    if(posSlash != -1):
+        line = line[0:posSlash]
     #remover maiusculas
-    if line01.isupper():
-        line01 = line01.lower()
+    if line.isupper():
+        line = line.lower()
+    #remover acentos
+    if(line != unidecode(line)):
+        line = unidecode(line)
+    #adicionar ao dicionario apenas se tiver 5 letras
+    if len(line) == 5:
+        dict_final(line,"w")
 
 dict_base.close()
 dict_final.close()
