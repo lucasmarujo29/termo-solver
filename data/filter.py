990f6da950906ceb73e60a218e38ac584afe8c2e
Lucas Ferreira Marinho <185864460+lucasmarujo29@users.noreply.github.com>
2026-03-10 13:35:04 -0300

    .

--- data/filter.py
+++ data/filter.py
@@ -1,33 +1,36 @@
-# este arquivo será responsável por filtrar as palavras do dicionário, ou seja, pegar somente as palavras que sejam válidas para o jogo
-# o dicionário que peguei do Libre Office possui a seguinte formatação:
-# o primeiro elemento é o número de palavras, depois tem cada palavra, algumas com um "palavra/categoria"
-# ou seja preciso:
-# 
-# 1. remover as categorias, ou seja, tudo que vier depois de uma barra (/) em cada palavra
-# 2. remover letras maiusculas, caracteres especiais, sendo acentos, hífens e etc
-# 3. remover palavras que não tenham exatamente 5 letras
-#
-import unidecode
+# # este arquivo será responsável por filtrar as palavras do dicionário, ou seja, pegar somente as palavras que sejam válidas para o jogo
+# # o dicionário que peguei do Libre Office possui a seguinte formatação:
+# # o primeiro elemento é o número de palavras, depois tem cada palavra, algumas com um "palavra/categoria"
+# # ou seja preciso:
+# # 
+# # 1. remover as categorias, ou seja, tudo que vier depois de uma barra (/) em cada palavra
+# # 2. remover letras maiusculas, caracteres especiais, sendo acentos, hífens e etc
+# # 3. remover palavras que não tenham exatamente 5 letras
+# #
+# import unidecode
+# import os
 
-dict_base = open("dicitonary.txt", "r")
-dict_final = open("dictionary_modified.txt", "w")
+# print(os.getcwd())
+
+# dict_base = open("data/dictionary.txt", "r", encoding="utf-8")
+# dict_final = open("data/dictionary_modified.txt", "w", encoding="utf-8")
 
 
-for line in dict_base:
-    line = line.strip()
-    #remover tag
-    posSlash = line.find("/")
-    if(posSlash != -1):
-        line = line[0:posSlash]
-    #remover maiusculas
-    if line.isupper():
-        line = line.lower()
-    #remover acentos
-    if(line != unidecode(line)):
-        line = unidecode(line)
-    #adicionar ao dicionario apenas se tiver 5 letras
-    if len(line) == 5:
-        dict_final(line,"w")
+# for line in dict_base:
+#     line = line.strip()
+#     #remover tag
+#     posSlash = line.find("/")
+#     if(posSlash != -1):
+#         line = line[0:posSlash]
+#     #remover maiusculas
+#     line = line.lower()
+#     #remover acentos
+#     if(line != unidecode.unidecode(line)):
+#         line = unidecode.unidecode(line)
+#     #adicionar ao dicionario apenas se tiver 5 letras
+#     if len(line) == 5:
+#         dict_final.write(line + "\n")
 
-dict_base.close()
-dict_final.close()
+# dict_base.close()
+# dict_final.close()
+
