e181261611f179effdcbaa481e62a5372b5395df
Lucas Ferreira Marinho <185864460+lucasmarujo29@users.noreply.github.com>
2026-04-21 21:19:18 -0300

    explicacao do codigo em comentários

--- data/filter.py
+++ data/filter.py
@@ -3,43 +3,41 @@
 # arquivo lexico: Léxico completo de palavras portuguesas de múltiplas fontes dicionárias (145.744 entradas)
 # arquivo conjugações: Todas as conjugações verbais (195.751 formas)
 # vou filtrar os dois
+# rodar esse arquivo cria o dicionario de cinco letras
 
 import unidecode
 
 
-def removeRepeatingLines():
+def removeRepeatingLines(): #falta implementar
     dict_final = open("data/words-5-letters.txt", "w", encoding="utf-8")
     aux = open("data/aux.txt", "w", encoding="utf-8")
-
+    # duas opções para remover as linhas repetidas:
+    # 1. ler o arquivo e só adicionar uma linha em outro arquivo caso a linha anterior ou a linha sucessiva seja diferente
+    # 2. ler o arquivo e fazer um set (estrutura de dado que não aceita elementos repetidos) e depois escrever o set inteiro em outro arquivo
     wordsFinal=set()
 
     aux.close()
     dict_final.close()
 
 def createMainList():
-    b1 = open("data/lexico.txt", "r", encoding="utf-8") 
-    b2 = open("data/conjugações.txt", "r", encoding="utf-8")
-    aux = open("data/aux.txt", "w", encoding="utf-8")
-    for line in b1:
-        line = line.strip()
-        #remover maiusculas
-        line = line.lower()
-        #remover acentos
+    b1 = open("data/lexico.txt", "r", encoding="utf-8") #abre o arquivo lexico para leitura
+    b2 = open("data/conjugações.txt", "r", encoding="utf-8") #abre o arquivo conjugações para leitura
+    aux = open("data/aux.txt", "w", encoding="utf-8") #abre o arquivo aux.txt para escrita, onde serão escritas as palavras filtradas
+    for line in b1: #for que percorre as linhas do arquivo
+        line = line.strip() #a função strip() remove os espaços em branco no inicio e final da string (que no caso é a linha do arquivo)
+        line = line.lower() #a função lower() transforma todas as letras da string em minúsculas
         if(line != unidecode.unidecode(line)):
             line = unidecode.unidecode(line)
-        #adicionar ao dicionario apenas se tiver 5 letras
+        #esse último trecho usa uma biblioteca com uma função para remover acentos
+        #como só adicionaremos palavras de 5 letras, usamos a função len() para verificar
         if len(line) == 5:
             aux.write(line + "\n")
-
-    for line in b2:
+    for line in b2: #REPETIÇÃO DO MESMO PROCESSO PARA O OUTRO ARQUIVO
         line = line.strip()
-        #remover maiusculas
         line = line.lower()
-        #remover acentos
         if(line != unidecode.unidecode(line)):
             line = unidecode.unidecode(line)
-        #adicionar ao dicionario apenas se tiver 5 letras
-        if (len(line) == 5) & (line.isalpha):
+        if (len(line) == 5) & (line.isalpha): #a função isalpha() verifica se a string é composta somente por letras, ou seja, não tem números ou caracteres especiais
             aux.write(line + "\n")
 
     b1.close()
